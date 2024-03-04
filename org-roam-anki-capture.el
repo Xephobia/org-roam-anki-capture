e24bb91470d7f8fa5059b95622a516c8d5dd325e
VNAT <xepjk@protonmail.com>
2024-03-04 14:51:25 +0100

    Add header

---
+++ org-roam-anki-capture.el
@@ -0,0 +1,12 @@
+;;; org-roam-anki-capture.el --- capture anki flashcards that links to org-roam notes  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  
+
+;; Author: Xephobia
+;;; Commentary:
+;; A way to capture Anki flashcards from org-roam nodes that links to where you were, using anki-editor
+;;; Code:
+
+(provide 'org-roam-anki-capture)
+
+;;; org-roam-anki-capture.el ends here
